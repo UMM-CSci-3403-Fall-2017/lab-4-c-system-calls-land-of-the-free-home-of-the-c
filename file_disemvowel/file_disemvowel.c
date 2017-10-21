aed843fa87faf180987c2993af81a3065f9cd5c4
casey369 <casey369@morris.umn.edu>
2017-10-21 14:04:29 -0500

    Makes progress on file_disemvowel.c

--- file_disemvowel/file_disemvowel.c
+++ file_disemvowel/file_disemvowel.c
@@ -1,8 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 #define BUF_SIZE 1024
 
+bool is_vowel(char c){
+	if(c == 'a' || c == 'A' || c == 'e' || c == 'E' || c == 'i' || c == 'I' || c == 'o' || c == 'O' || c == 'u' || c == 'U'){
+		return true;
+	}else{
+		return 	false;
+	}
+}
+
+int copy_non_vowels(int num_chars, char* in_buf, char* out_buf){
+	int i, j=0;
+	for(i = 0; i < num_chars;i++){
+		if(!is_vowel(in_buf[i])){
+			out_buf[j] = in_buf[i];
+			j++;
+		}
+	}
+	out_buf[j] = '\0';
+	return j;
+}
 
 void disemvowel(FILE* inputFile, FILE* outputFile){
 	char buffer[BUF_SIZE + 1];
@@ -16,19 +36,19 @@ int main(int argc, char *argv[]){
 	FILE *inputFile;
 	FILE *outputFile;
 	if(argc == 0){
-	inputFile = stdin;
-	outputFile = stdout;
-	disemvowel(inputFile,outputFile);
-	return 0;
+		inputFile = stdin;
+		outputFile = stdout;
+		disemvowel(inputFile,outputFile);
+		return 0;
 	} else if(argc == 1){
-	inputFile = fopen(argv[0],"r");
-	outputFile = stdout;
-	disemvowel(inputFile,outputFile);
-	return 0;
+		inputFile = fopen(argv[0],"r");
+		outputFile = stdout;
+		disemvowel(inputFile,outputFile);
+		return 0;
 	} else if(argc == 2){
-	inputFile = fopen(argv[0],"r");
-	outputFile = fopen(argv[1],"w");
-	disemvowel(inputFile,outputFile);
-	return 0;
+		inputFile = fopen(argv[0],"r");
+		outputFile = fopen(argv[1],"w");
+		disemvowel(inputFile,outputFile);
+		return 0;
 	}
 }
