a39a81ee80f2ffc366adabae9c1e36f631fd3367
casey369 <casey369@morris.umn.edu>
2017-10-22 16:26:00 -0500

    Adds summarize_tree.sh and finishes it

---
+++ summarize_tree/summarize_tree.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+find $1 -type d | wc -l > dirs 
+find $1 -type f | wc -l > files
+read dirs < dirs
+read files < files
+echo "processed all the files from $1"
+echo "There were $dirs directories."
+echo "there were $files regular files."
