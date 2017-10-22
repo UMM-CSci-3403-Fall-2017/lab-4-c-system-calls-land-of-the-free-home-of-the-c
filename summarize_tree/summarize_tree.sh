08558cdec2fe34c4c2fe3b722053a4054f793a54
casey369 <casey369@morris.umn.edu>
2017-10-22 17:30:54 -0500

    summarize_tree.sh now removes temp files

--- summarize_tree/summarize_tree.sh
+++ summarize_tree/summarize_tree.sh
@@ -4,6 +4,8 @@ find $1 -type d | wc -l > dirs
 find $1 -type f | wc -l > files
 read dirs < dirs
 read files < files
+rm dirs;
+rm files;
 echo "processed all the files from $1"
 echo "There were $dirs directories."
 echo "there were $files regular files."
